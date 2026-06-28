778786742310a08391d65816f88c2aeafc826a3c
unknown <vaghmshisneha@example.com>
2026-06-28 19:43:29 +0530

    Day 7: Added JavaScript basics and DOM manipulation

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("JavaScript Connected");
+
+
+function sayHello(){
+    alert("Hello Sneha!");
+}
+function showMessage(){
+    alert("Welcome to my Portfolio!")
+}
+
+function changeTitle(){
+    document.getElementById("hero-title").innerHTML="Welcome to My portfolio!";
+}
+function changeColor() {
+    document.getElementById("hero-title").style.color = "red";
+}
